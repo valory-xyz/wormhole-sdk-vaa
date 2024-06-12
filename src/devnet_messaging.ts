b95eb8a0f92499eb2b1b6425c0b2a851e1ef5052
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2024-06-12 09:38:56 +0100

    chore: vaa for the governance transfer instruction

--- src/devnet_messaging.ts
+++ src/devnet_messaging.ts
@@ -12,16 +12,18 @@ import solana from "@wormhole-foundation/sdk/solana";
 import { getStuff } from "./helpers.js";
 
 (async function () {
-  const wh = await wormhole("Devnet", [solana], {
-    chains: {
-      Solana: {
-        contracts: {
-          coreBridge: "Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o",
-        },
-        rpc: "https://api.devnet.solana.com",
-      },
-    },
-  });
+  const wh = await wormhole("Testnet", [solana]);
+
+//  const wh = await wormhole("Devnet", [solana], {
+//    chains: {
+//      Solana: {
+//        contracts: {
+//          coreBridge: "Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o",
+//        },
+//        rpc: "https://api.devnet.solana.com",
+//      },
+//    },
+//  });
 
   const chainCtx = wh.getChain("Solana");
   const coreBridge = await chainCtx.getWormholeCore();
@@ -33,11 +35,11 @@ import { getStuff } from "./helpers.js";
     emitter: new UniversalAddress(
       "000000000000000000000000471b3f60f08c50dd0ecba1bcd113b66fcc02b63d"
     ),
-    sequence: 1n,
+    sequence: 7n,
   };
   const apiUrl = "https://api.testnet.wormholescan.io";
   const vaa = await api.getVaa(apiUrl, whm, "Uint8Array");
-  //console.log(vaa);
+  console.log(vaa);
 
   // Get local signer and parse the address
   const {
@@ -53,11 +55,10 @@ import { getStuff } from "./helpers.js";
   // submit verify txs
   console.log(await signSendWait(chainCtx, verifyTxs, signer));
 
-  // Two signature verification tx-s:
-  // 2YhtHGjwRWonPby8UEXYTcHYps1sgsnhAgPCktbtWR5qoBT34QuG44Bps9PYTK9WfjqXweVF1cKiu58ZGDe6sGha
-  // 3hXEguRMAL5oZsJnPi56pCj2kGsCxNYo77QLMqgofvgqe4ia2oBM8tAbwf2qhgYi4vHfanCKmiDcXEXfEX5Vhpuc
+  // Signature verification tx:
+  // 56CB6tNVox2ebuELi6d46Hoh1fAYyWkRDfXJxWwxkHE9tuaemWs4EYnfZFauL4YmPq7SxySSuAbXhtktfNrPmBss
 
   // postVAA tx:
-  // 3vVzQmnX4qnzBu4KaAZ9zg5Q18z31ycyvVEK5oG152waNNRBV8DknAPskqsjTiFtcbou5VKnB3dMvJtYMfe2dkWK
-  // Created "posted" account: Bq8zzYk9sAxFpaWUCUsZz16XgrD27QMNhPYR9wkirh5f
+  // BYZbDuVFx2ZESobKbSm2tQZG3K4ZWSUF749F1TsGCM3S7tFqTby3e27ijEmP7YSLZhetaVLBt3FQWK8AQUFDNhj
+  // Created "posted" account: AdKqXRW51SyZgepKMs2x77kNYMv4CQfsjD7vResES9EQ
 })();
